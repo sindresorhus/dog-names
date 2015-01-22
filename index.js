5c784efc5bbeed6ae6f58b251c1aa0ed88d600b5
Sindre Sorhus <sindresorhus@gmail.com>
2015-01-23 03:29:17 +0800

    simplify

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-var uniqueRandom = require('unique-random');
+var uniqueRandomArray = require('unique-random-array');
 var femaleDogNames = require('./female-dog-names.json');
 var maleDogNames = require('./male-dog-names.json');
 
@@ -9,22 +9,10 @@ femaleDogNames.forEach(function (el, i) {
 	allDogNames.push(el, maleDogNames[i]);
 });
 
-var femaleRandom = uniqueRandom(0, femaleDogNames.length - 1);
-var maleRandom = uniqueRandom(0, femaleDogNames.length - 1);
-var allRandom = uniqueRandom(0, allDogNames.length - 1);
-
 exports.female = femaleDogNames;
 exports.male = maleDogNames;
 exports.all = allDogNames;
 
-exports.femaleRandom = function () {
-	return femaleDogNames[femaleRandom()];
-};
-
-exports.maleRandom = function () {
-	return maleDogNames[maleRandom()];
-};
-
-exports.allRandom = function () {
-	return allDogNames[allRandom()];
-};
+exports.femaleRandom = uniqueRandomArray(femaleDogNames);
+exports.maleRandom = uniqueRandomArray(maleDogNames);
+exports.allRandom = uniqueRandomArray(allDogNames);
